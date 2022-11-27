b2883d340d62366cd412828b23c350bad18071ba
jesnagbg <j.m.pettersson@hotmail.se>
2022-11-27 15:07:20 +0100

    darkmode works

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 window.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    gradientBorder();
+    addEventListeners();
 }
 
-var image = document.getElementsByClassName("image2");
-image.src = "image1.jpg"
+function addEventListeners() {
+    toggleLightMode();
+
+}
+
+function toggleLightMode() {
+    const checkbox = document.getElementById('checkbox');
+    const body = document.querySelector('body');
+
+    checkbox.addEventListener('change', function() {
+        body.classList.toggle('dark');
+    });
+}
+
+
+
+
+//var image = document.getElementsByClassName("image2");
+//image.src = "image1.jpg"

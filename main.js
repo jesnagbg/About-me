8c2a0b11e7947182e7999d1e7e0c3a70d1f3a992
jesnagbg <j.m.pettersson@hotmail.se>
2022-11-27 18:02:49 +0100

    eyes can blink

--- main.js
+++ main.js
@@ -15,11 +15,19 @@ function toggleLightMode() {
 
     checkbox.addEventListener('change', function() {
         body.classList.toggle('dark');
+        displayCorrectImage();
     });
 }
 
+function displayCorrectImage() {
+    const imgEyesOpen = document.getElementById("profile-pic").src="assets/drawingme.png";
+    const imgEyesClosed = document.getElementById("profile-pic").src="assets/drawingmesleep.png";
+
+    if (document.getElementById('checkbox').checked) {
+        document.getElementById("profile-pic").src="assets/drawingme.png";
+    } else {
+        document.getElementById("profile-pic").src="assets/drawingmesleep.png";
+    }
+}
 
 
-
-//var image = document.getElementsByClassName("image2");
-//image.src = "image1.jpg"

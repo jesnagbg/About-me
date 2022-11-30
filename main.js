95f5a691f268ff3b2fcbaa6abb4575e5d214f277
jesnagbg <j.m.pettersson@hotmail.se>
2022-11-30 20:11:22 +0100

    button def works

--- main.js
+++ main.js
@@ -10,11 +10,16 @@ function addEventListeners() {
     listenPageButton();
 }
 
+
 function listenCheckbox() {
     const checkbox = document.getElementById('checkbox');
     checkbox.addEventListener('change', toggleTheme);
 }
 
+/**
+ * 
+ * @param {change} event 
+ */
 function toggleTheme(event) {
     const body = document.querySelector('body');
     const profilePic = document.getElementById("profile-pic");
@@ -35,17 +40,32 @@ function listenPageButton() {
     button.onclick = changeButtonDirection;
 }
 
-function changeButtonDirection() {    
+function changeButtonDirection() {  
+    const buttonIcon = document.getElementById('button-icon');
     const buttonLink = document.getElementById('scroll-down');
     let buttonHref = buttonLink.getAttribute("href");
-
+    
     if (buttonHref === "#skills") {
         buttonLink.setAttribute('href', '#top-page');
+        removeClasses();
+        buttonIcon.classList.add('fa-solid', 'fa-angles-down', 'linked-logo');
         console.log("hi")
+    } else if (buttonHref === "#top-page") {
+        buttonLink.setAttribute('href', '#skills');
+        removeClasses();
+        buttonIcon.classList.add('fa-solid', 'fa-angles-up', 'linked-logo');
+        console.log("no")
     } else {
         buttonLink.setAttribute('href', '#skills');
-        console.log("no")
+        removeClasses();
+        buttonIcon.classList.add('fa-solid', 'fa-angles-up', 'linked-logo');
+        console.log('hej')
     }
+}
+
+function removeClasses() {
+    const buttonIcon = document.getElementById('button-icon');
+    buttonIcon.classList = "";
 }
 
 
